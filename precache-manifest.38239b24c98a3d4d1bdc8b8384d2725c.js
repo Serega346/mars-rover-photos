7fd717b8792ccdeddb9f6596d2047c512fd908cc
Serhii Khomyn <serghomyn@gmail.com>
2020-10-21 21:58:35 +0200

    Updates

---
+++ precache-manifest.38239b24c98a3d4d1bdc8b8384d2725c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced229a747587d0fb585efecb3bdd9d6",
+    "url": "/mars-rover-photos/index.html"
+  },
+  {
+    "revision": "8dde419c42682eded8d8",
+    "url": "/mars-rover-photos/static/css/main.d76a8bc0.chunk.css"
+  },
+  {
+    "revision": "808ec6bbab93b842b9ea",
+    "url": "/mars-rover-photos/static/js/2.49b04c71.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/mars-rover-photos/static/js/2.49b04c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dde419c42682eded8d8",
+    "url": "/mars-rover-photos/static/js/main.fc132fed.chunk.js"
+  },
+  {
+    "revision": "eeb6c35a36e45bbf353d",
+    "url": "/mars-rover-photos/static/js/runtime-main.8dc5c733.js"
+  }
+]);
